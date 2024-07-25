63a810511b421cea5a046f599d29d43d310aa9c7
Anurag Anand <114821800+Anurag990@users.noreply.github.com>
2024-07-25 12:15:16 +0530

    Add files via upload

---
+++ CollegeManagmentSystem/CollegeManagmentSystem/src/main/java/com/codingshuttle/tutorial/dataMapping/CollegeManagmentSystem/CollegeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ CollegeManagmentSystem/CollegeManagmentSystem/src/test/java/com/codingshuttle/tutorial/dataMapping/CollegeManagmentSystem/CollegeManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
