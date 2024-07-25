7664d4276d2e2e413284bd158e00057e88a93eb2
Anurag Anand <114821800+Anurag990@users.noreply.github.com>
2024-07-25 12:13:54 +0530

    Delete CollegeManagmentSystem/CollegeManagmentSystem directory

--- CollegeManagmentSystem/CollegeManagmentSystem/src/main/java/com/codingshuttle/tutorial/dataMapping/CollegeManagmentSystem/repositories/StudentRepo.java
+++
@@ -1,1 +0,0 @@
-package com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem.repositories;import com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem.entites.Student;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface StudentRepo extends JpaRepository<Student,Long> {}
--- CollegeManagmentSystem/CollegeManagmentSystem/src/main/java/com/codingshuttle/tutorial/dataMapping/CollegeManagmentSystem/repositories/SubjectRepo.java
+++
@@ -1,1 +0,0 @@
-package com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem.repositories;import com.codingshuttle.tutorial.dataMapping.CollegeManagmentSystem.entites.Subject;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;@Repositorypublic interface SubjectRepo extends JpaRepository<Subject,Long> {}
